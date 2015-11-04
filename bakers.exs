de6d886406c86f3c2f15f625eed8034eed4cf65a
Josh Chapman <chapm250@morris.umn.edu>
2015-11-04 05:57:39 -0600

    Update bakers.exs

--- bakers.exs
+++ bakers.exs
@@ -1,2 +1,36 @@
 ####Creating bakers algorithm
-####Checking to see if it commits correctly
+
+defmodule Bakers do 
+	def server do 
+		receive do 
+			{ sender, n} ->
+				send sender, {:ok, serverCalc(n)}
+			end
+		end
+
+		def serverCalc(0), do: 0
+		def serverCalc(1), do: 1
+		def serverCalc(n), do: serverCalc(n-1) + serverCalc(n-2)
+	end
+
+
+	defmodule Manager do 
+
+
+		def run(cust, listOfPid) do
+			if length(cust) > 0  do
+				send List.first(listOfPid), {self, List.first(cust)}
+				receive do 
+					{:ok, answer} ->
+						List.insert_at(listOfPid, 0, spawn(Bakers, :server, []))
+						IO.puts answer
+					end
+
+					run(List.delete_at(cust, 0), List.delete_at(listOfPid, 0))
+				end
+			end
+		end
+
+		listOfServ = [spawn(Bakers, :server, []), spawn(Bakers, :server, [])]
+
+		Manager.run([10, 9, 8 ], listOfServ)
